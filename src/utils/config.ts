fe29eb3db356d5ca1ae7fae9a5bc8ea483bd56e2
aeither <36173828+aeither@users.noreply.github.com>
2024-08-23 21:07:18 +0200

    use edu chain

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,6 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-
 import { cookieStorage, createStorage } from "wagmi";
-import { base } from "wagmi/chains";
+import { Chain } from "wagmi/chains";
 
 // Get projectId from https://cloud.walletconnect.com
 export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
@@ -15,8 +14,29 @@ export const metadata = {
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
+// Define Open Campus Codex chain
+const openCampusCodex: Chain = {
+  id: 656476,
+  name: "Open Campus Codex",
+  nativeCurrency: {
+    decimals: 18,
+    name: "EDU",
+    symbol: "EDU",
+  },
+  rpcUrls: {
+    public: { http: ["https://rpc.open-campus-codex.gelato.digital"] },
+    default: { http: ["https://rpc.open-campus-codex.gelato.digital"] },
+  },
+  blockExplorers: {
+    default: {
+      name: "Blockscout",
+      url: "https://opencampus-codex.blockscout.com/",
+    },
+  },
+};
+
 // Create wagmiConfig
-const chains = [base] as const;
+const chains = [openCampusCodex] as const;
 export const config = defaultWagmiConfig({
   chains,
   projectId,
